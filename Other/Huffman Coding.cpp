63b5df47e9c660b23cffdb95914198425eea126f
RandomUsername96 <koorweetzaah2@gmail.com>
2016-03-07 20:41:57 +0100

    Test SublimeGit

---
+++ Other/Huffman Coding.cpp
@@ -0,0 +1,58 @@
+// RandomUsername (Nikola Jovanovic)
+// Huffman Coding
+// Build: O(N) [N = alphabet size]
+// Encode/Dedode: O(H) on average [H = entropy]
+
+#include <bits/stdc++.h>
+#define DBG false
+#define debug(x) if(DBG) printf("(ln %d) %s = %d\n", __LINE__, #x, x);
+#define lld long long
+#define ff(i,a,b) for(int i=a; i<=b; i++)
+#define fb(i,a,b) for(int i=a; i>=b; i--)
+#define par pair<int, int>
+#define fi first
+#define se second
+#define mid (l+r)/2
+#define INF 1000000000
+#define MAXN 10005
+#define MOD 1000000007
+
+using namespace std;
+
+struct TreeNode
+{
+
+};
+TreeNode* root;
+
+map<char, double> freq;
+
+void freq_analysis(string s)
+{
+
+}
+
+void build()
+{
+
+}
+
+char encode_char(char ch)
+{
+
+}
+
+string encode(string s)
+{
+
+}
+
+string decode(string s)
+{
+
+}
+
+int main()
+{
+
+}
